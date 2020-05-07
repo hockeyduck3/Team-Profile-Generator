d4073df3bbea00ebaf3b2cda498e8c77e510b003
LJ Spencer <hockeyduck3@gmail.com>
2020-05-07 03:06:20 -0600

    Added a module for some basic CSS styling that will be added after the user is done building their team.

---
+++ style.js
@@ -0,0 +1,20 @@
+const style =
+`.jumbotron {
+    background-color: #444;
+    border-radius: 0;
+    box-shadow: 0px 4px 7px -3px #444;
+    color: white;
+}
+
+.card {
+    border-color: rgb(196, 196, 196);
+    box-shadow: 2px 3px 5px -2px black;
+    margin-right: 25px;
+    margin-top: 35px;
+}
+
+.text-center {
+    font-family: 'Yellowtail', cursive;
+}`;
+
+module.exports = style;
